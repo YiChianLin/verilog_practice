41afd87adb4c1bc7bf8d10fc0ee451a72347f211
YiChianLin <heydanny316@hotmail.com>
2023-03-07 18:44:09 +0800

    Add MMS module

--- HW1/MMS_4num.v
+++ HW1/MMS_4num.v
@@ -7,8 +7,26 @@ input  [7:0] number2;
 input  [7:0] number3;
 output [7:0] result; 
 
-/*
-	Write Your Design Here ~
-*/
+reg cmp1, cmp2, result;
 
+always @(select or number0 or number1 or number2 or number3) begin
+	// select = 0 choose maximun
+	if (select) begin
+		cmp1 = (number0 <= number1) ? number0 : number1;
+		cmp2 = (number2 <= number3) ? number2 : number3;
+	end
+	else begin
+		cmp1 = (number0 <= number1) ? number1 : number0;
+		cmp2 = (number2 <= number3) ? number3 : number2;
+	end
+end
+
+always @(select or cmp1 or cmp2) begin
+	if (select) begin
+		result = (cmp1 <= cmp2) ? cmp1 : cmp2;
+	end
+	else begin
+		result = (cmp1 <= cmp2) ? cmp2 : cmp1;
+	end
+end
 endmodule
