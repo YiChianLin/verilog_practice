26bd2625f3f168f19926c18573aef11575b3db14
YiChianLin <heydanny316@hotmail.com>
2023-03-10 21:11:45 +0800

    Use MMS_4num module create 8 number

--- HW1/MMS_8num.v
+++ HW1/MMS_8num.v
@@ -1,4 +1,4 @@
-
+`include "MMS_4num.v"
 module MMS_8num(result, select, number0, number1, number2, number3, number4, number5, number6, number7);
 
 input        select;
@@ -10,10 +10,19 @@ input  [7:0] number4;
 input  [7:0] number5;
 input  [7:0] number6;
 input  [7:0] number7;
-output [7:0] result; 
+output reg[7:0] result; 
 
-/*
-	Write Your Design Here ~
-*/
+wire[7:0] mms_4n_cmp1, mms_4n_cmp2; 
+MMS_4num mms_4n_m1(.result(mms_4n_cmp1), .select(select), .number0(number0), .number1(number1), .number2(number2), .number3(number3));
+MMS_4num mms_4n_m2(.result(mms_4n_cmp2), .select(select), .number0(number4), .number1(number5), .number2(number6), .number3(number7));
+
+always @(*) begin
+	if (select) begin
+		result = (mms_4n_cmp1 < mms_4n_cmp2) ? mms_4n_cmp1 : mms_4n_cmp2;
+	end
+	else begin
+		result = (mms_4n_cmp1 < mms_4n_cmp2) ? mms_4n_cmp2 : mms_4n_cmp1;
+	end
+end
 
 endmodule
