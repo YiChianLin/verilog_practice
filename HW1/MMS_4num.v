0a25b81fdc0ec16ff597c05b1be2cd3017764b59
YiChianLin <heydanny316@hotmail.com>
2023-03-31 17:32:56 +0800

    Modify the 4num with turth table method

--- HW1/MMS_4num.v
+++ HW1/MMS_4num.v
@@ -5,20 +5,30 @@ input  [7:0] number0;
 input  [7:0] number1;
 input  [7:0] number2;
 input  [7:0] number3;
-output [7:0] result; 
+output reg[7:0] result; 
 
-reg[7:0] cmp1, cmp2, result; 
+reg[7:0] cmp1, cmp2; 
 
-always @(select or number0 or number1 or number2 or number3) begin
-	if (select) begin
-		cmp1 = (number0 < number1) ? number0 : number1;
-		cmp2 = (number2 < number3) ? number2 : number3;
-		result = (cmp1 < cmp2) ? cmp1 : cmp2;
-	end
-	else begin
-		cmp1 = (number0 < number1) ? number1 : number0;
-		cmp2 = (number2 < number3) ? number3 : number2;
-		result = (cmp1 < cmp2) ? cmp2 : cmp1;
-	end
+always @(select, number0, number1) begin
+	case ({select, (number0 < number1)})
+		2'b00 : cmp1 = number0;
+		2'b11 : cmp1 = number0;
+		default: cmp1 = number1;
+	endcase
+end
+always @(select, number2, number3) begin
+	case ({select, (number2 < number3)})
+		2'b00 : cmp2 = number2;
+		2'b11 : cmp2 = number2;
+		default: cmp2 = number3;
+	endcase
+end
+
+always @(cmp1, cmp2) begin
+	case ({select, (cmp1 < cmp2)})
+		2'b00 : result = cmp1;
+		2'b11 : result = cmp1;
+		default: result = cmp2;
+	endcase
 end
 endmodule
