e6cc56020383241d05322e7fdc5cb33e13cf7039
YiChianLin <heydanny316@hotmail.com>
2023-03-31 17:36:57 +0800

    Modify the 8num with turth table method

--- HW1/MMS_8num.v
+++ HW1/MMS_8num.v
@@ -1,4 +1,3 @@
-`include "MMS_4num.v"
 module MMS_8num(result, select, number0, number1, number2, number3, number4, number5, number6, number7);
 
 input        select;
@@ -13,16 +12,18 @@ input  [7:0] number7;
 output reg[7:0] result; 
 
 wire[7:0] mms_4n_cmp1, mms_4n_cmp2; 
+wire cmp_bit;
 MMS_4num mms_4n_m1(.result(mms_4n_cmp1), .select(select), .number0(number0), .number1(number1), .number2(number2), .number3(number3));
 MMS_4num mms_4n_m2(.result(mms_4n_cmp2), .select(select), .number0(number4), .number1(number5), .number2(number6), .number3(number7));
 
-always @(*) begin
-	if (select) begin
-		result = (mms_4n_cmp1 < mms_4n_cmp2) ? mms_4n_cmp1 : mms_4n_cmp2;
-	end
-	else begin
-		result = (mms_4n_cmp1 < mms_4n_cmp2) ? mms_4n_cmp2 : mms_4n_cmp1;
-	end
+assign cmp_bit = (mms_4n_cmp1 < mms_4n_cmp2);
+
+always @(*) begin	
+	case ({select, cmp_bit})
+		2'b00: result = mms_4n_cmp1;
+		2'b11: result = mms_4n_cmp1;
+		default: result = mms_4n_cmp2;
+	endcase
 end
 
 endmodule
